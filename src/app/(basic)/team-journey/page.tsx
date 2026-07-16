ef254e9dcb6fcd4881455354ad543c50e00b7725
RashmiMahar <rashmi.mahar@mobilecoderz.com>
2026-07-16 21:49:36 +0530

    issue fixed

--- src/app/(basic)/team-journey/page.tsx
+++ src/app/(basic)/team-journey/page.tsx
@@ -1,13 +1,17 @@
 "use client";
 import withProtectedRoute from "@/src/components/HOCs/withProtectedRoute";
 import TeamJourney from "@/src/modules/MyTeamJourneyModule/Components/TeamJourney/TeamJourney";
+import useGetMtjListQuery from "@/src/modules/MyTeamJourneyModule/Hooks/useGetMtjListQuery";
 import React, { Suspense } from "react";
 
 function TeamJourneyPage() {
+  const { data: mtjListData } = useGetMtjListQuery();
+
+  const rituals = mtjListData?.data?.team_rituals || [];
   return (
     <>
       <Suspense fallback={"Loading..."}>
-        <TeamJourney />
+        <TeamJourney rituals={rituals} />
       </Suspense>
     </>
   );
